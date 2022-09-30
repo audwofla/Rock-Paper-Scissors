fea806c1a174deff41e144d889c9067bd8fb6877
Jeremy <jeremyjerim@gmail.com>
2022-09-30 11:42:09 -0700

    Add javascript structure and comments

---
+++ javascript.js
@@ -0,0 +1,21 @@
+//randomly return rock paper or scissors
+function getComputerChoice() {
+
+}
+
+const playerSelection = prompt();
+const copmuterSelection = getComputerChoice();
+//plays a round of rock paper scissors
+//takes two parameters and returns a string that declares the winner
+//"You lose! Paper beats Rock!"
+//make playerSelection parameter case-insensitive (rock, ROCK, or RocK are all valid results)
+function gameRound(playerSelection, copmuterSelection) {
+
+}
+
+function game() {
+  for (let i = 0, i < 5, i++) {
+
+  }
+
+}
