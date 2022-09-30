357aa82da50e6733c0458fe5e475a659b699d307
Jeremy <jeremyjerim@gmail.com>
2022-09-30 14:51:00 -0700

    Add logging inputs, winloss, rounds, best of three

--- javascript.js
+++ javascript.js
@@ -8,11 +8,11 @@ function getComputerChoice() {
   }else if (choice > .67) {
     choice = "Scissors";
   }
-  return choice
-
+  return choice;
 }
-let playerSelection = ""
-let computerSelection = ""
+let playerSelection = "";
+let computerSelection = "";
+let winner = "";
 
 //plays a round of rock paper scissors
 //takes two parameters and returns a string that declares the winner
@@ -20,43 +20,72 @@ let computerSelection = ""
 //make playerSelection parameter case-insensitive (rock, ROCK, or RocK are all valid results)
 function gameRound(playerSelection, computerSelection) {
   computerSelection = getComputerChoice();
+  console.log("You: " + playerSelection)
+  console.log("Computer: " + computerSelection)
   playerSelection = playerSelection.toUpperCase();
   if (playerSelection == "ROCK") {
     if (computerSelection == "Rock") {
       console.log("It's a tie")
+      winner = "Tie";
     }else if (computerSelection == "Paper") {
       console.log("You lose! Paper beats rock!")
+      winner = "Lose";
     }else if (computerSelection == "Scissors") {
       console.log("You win! Rock beats scissors!")
+      winner = "Win";
     }
   }else if (playerSelection == "PAPER") {
     if (computerSelection == "Paper") {
       console.log("It's a tie")
+      winner = "Tie";
     }else if (computerSelection == "Scissors") {
       console.log("You lose! Scissors beats paper!")
+      winner = "Lose";
     }else if (computerSelection == "Rock") {
       console.log("You win! Paper beats rock!")
+      winner = "Win";
     }
   }else if (playerSelection == "SCISSORS") {
     if (computerSelection == "Scissors") {
       console.log("It's a tie")
+      winner = "Tie";
     }else if (computerSelection == "Rock") {
       console.log("You lose! Rock beats scissor!")
+      winner = "Lose";
     }else if (computerSelection == "Paper") {
       console.log("You win! Scissors beats paper!")
+      winner = "Win";
     }
+  return winner;
   }      
-
-
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
+  let win = 0;
+  let loss = 0;
+  for (let i = 1; i < 6; i++) {
     let playerSelection = prompt();
     let computerSelection = getComputerChoice();
     gameRound(playerSelection, computerSelection);
-
+    if (winner == "Win") {
+      win++;
+    }else if (winner == "Lose") {
+      loss++;
+    }else if (winner == "Tie") {
+      --i;
+    }
+    console.log("Round:", i);
+    console.log("Win:", win, "Loss:", loss);
+    if (win == 3) {
+      console.log("You win the best of three!");
+    }else if (loss == 3) {
+      console.log("You lose the best of three!");
+    }
   }
+
+
+
+  
 //use console.log() to display winner
 
 }
